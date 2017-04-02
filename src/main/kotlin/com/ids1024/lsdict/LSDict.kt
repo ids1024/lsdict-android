c62d192451204a537db622264c28c726cf7ba374
Ian Douglas Scott <ian@iandouglasscott.com>
2017-04-02 14:29:20 -0700

    Make search work

--- src/main/kotlin/com/ids1024/lsdict/LSDict.kt
+++ src/main/kotlin/com/ids1024/lsdict/LSDict.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.database.sqlite.SQLiteDatabase
 import android.widget.SimpleCursorAdapter
 import android.view.Menu
+import android.content.Context
+import android.app.SearchManager
+import android.widget.SearchView
+import android.content.Intent
 
 public class LSDict : ListActivity() {
     private lateinit var db: LSDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-	db = LSDatabase(this)
-        search("a")
+        db = LSDatabase(this)
+        handleIntent(intent)
     }
     private fun search(word: String) {
         val term = word.toLowerCase()
@@ -23,6 +27,18 @@ public class LSDict : ListActivity() {
     }
     override public fun onCreateOptionsMenu(menu: Menu) : Boolean {
         menuInflater.inflate(R.menu.main, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
         return true
+    }
+    override protected fun onNewIntent(intent: Intent) {
+        handleIntent(intent)
+    }
+    private fun handleIntent(intent: Intent) {
+        //if (intent.action == Intent.ACTION_SEARCH) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            search(intent.getStringExtra(SearchManager.QUERY))
+        }
     }
 }
