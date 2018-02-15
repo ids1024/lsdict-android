65a550a123940e8416556f711e1624db3b241a7c
Ian Douglas Scott <ian@iandouglasscott.com>
2018-02-15 12:19:58 -0800

    Update lewis.db (with ^ characters removed)

--- src/main/kotlin/com/ids1024/lsdict/LSDatabase.kt
+++ src/main/kotlin/com/ids1024/lsdict/LSDatabase.kt
@@ -5,7 +5,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteQueryBuilder
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
-val DB_VERSION = 3
+val DB_VERSION = 4
 
 public class LSDatabase(context: Context) : SQLiteAssetHelper(context, "lewis.db", null, DB_VERSION) {
     init {
