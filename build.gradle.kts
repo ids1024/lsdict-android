aeb6f303f3cb86d2fe1c7e07c2093c5706245c2a
Ian Douglas Scott <ian@iandouglasscott.com>
2025-10-02 21:17:29 -0700

    Migrate build.gradle -> build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 buildscript {
-    ext.kotlin_version = "2.0.10"
+    val kotlin_version by extra("2.0.10")
 }
 
 plugins {
+    val kotlin_version : String by rootProject.extra
     id("com.android.application") version "8.13.0"
-    id("org.jetbrains.kotlin.android") version "$kotlin_version"
+    id("org.jetbrains.kotlin.android") version kotlin_version
 }
 
 kotlin {
@@ -17,16 +18,16 @@ android {
     compileSdkVersion(35)
 
     buildFeatures {
-        viewBinding(true)
+        viewBinding = true
     }
 
     defaultConfig {
         minSdkVersion(21)
-        targetSdk(34)
+        targetSdk = 34
     }
 
     signingConfigs {
-        release {
+        register("release") {
             storeFile = file("release.keystore")
             storePassword = System.getenv("KEYPWD")
             keyAlias = "upload"
@@ -35,13 +36,13 @@ android {
     }
 
     buildTypes {
-        release {
-            minifyEnabled = false
-            signingConfig = signingConfigs.release
+        getByName("release") {
+            isMinifyEnabled = false
+            //signingConfig = signingConfigs.release
             proguardFile(getDefaultProguardFile("proguard-android.txt"))
         }
     }
-    namespace "com.ids1024.lsdict"
+    namespace = "com.ids1024.lsdict"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_22
@@ -58,11 +59,10 @@ repositories {
     google()
 }
 
-configurations {
-    ktlint
-}
+val ktlint: Configuration by configurations.creating
 
 dependencies {
+    val kotlin_version : String by rootProject.extra
     ktlint("com.pinterest.ktlint:ktlint-cli:1.7.1")
     //noinspection GradleDependency
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
@@ -71,18 +71,18 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
 }
 
-tasks.register("ktlint", JavaExec) {
+tasks.register<JavaExec>("ktlint") {
     group = "verification"
     description = "Check Kotlin code style."
-    classpath = configurations.ktlint
+    classpath = ktlint
     mainClass = "com.pinterest.ktlint.Main"
-    args "src/**/*.kt"
+    args("src/**/*.kt")
 }
 
-tasks.register("ktlintFormat", JavaExec) {
+tasks.register<JavaExec>("ktlintFormat") {
     group = "formatting"
     description = "Fix Kotlin code style deviations."
-    classpath = configurations.ktlint
+    classpath = ktlint
     mainClass = "com.pinterest.ktlint.Main"
-    args "-F", "src/**/*.kt"
+    args("-F", "src/**/*.kt")
 }
